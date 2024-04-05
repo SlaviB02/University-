fab4d0d5d9f2c0d5dc7b8ca6f1e0c1d8dc0148d2
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-04-05 10:39:40 +0300

    Add files via upload

---
+++ САА/Домашна_САА_СлавиБожилов_СИ659.cs
@@ -0,0 +1,57 @@
+﻿int NOD(int a, int b)
+{
+
+    if(a != b)
+    {
+        if (a > b)
+        {
+            return NOD(a - b, b);
+        }
+        else
+        {
+            return NOD(a, b - a);
+        }
+    }
+    return a;
+}
+int NumbersSum(int n)
+{
+    if(n==1)
+    {
+        return 1;
+    }
+    return n+NumbersSum(n-1);
+}
+void Row(int n)
+{
+  if(n<0)
+    {
+        return;
+    }
+    Row(n - 1);
+    Console.Write(n + " ");
+}
+void ReverseRow(int n)
+{
+    Console.Write(n + " ");
+    if(n==0)
+    {
+        return;
+    }
+    ReverseRow(n - 1);
+}
+
+    Console.WriteLine("НОД рекусия: ");
+    Console.WriteLine(NOD(15, 45));
+    Console.WriteLine(NOD(40, 4));
+    Console.WriteLine("Сума n числа:");
+    Console.WriteLine(NumbersSum(10));
+    Console.WriteLine("Редица 0-n");
+    Row(15);
+    Console.WriteLine();
+    Console.WriteLine("Редица n-0");
+    ReverseRow(15);
+
+
+
+
