ae924de2b05c098268f5fb87205c3ce3b307f6a8
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-05-23 00:15:45 +0300

    Add files via upload

---
+++ САА/Домашна2_САА_СлавиБожилов_СИ659.cs
@@ -0,0 +1,45 @@
+﻿void Row(int n,int current)
+{
+   
+    if (current<n) 
+    {
+        Console.Write(current + " ");
+        Row(n, current + 1);
+    }
+    else
+    {
+        if (n == 0) return;
+        Console.Write(n + " ");
+        Row(n - 1, current);
+    }
+   
+  
+
+}
+void ReverseRow(int n)
+{
+    Console.Write(n + " ");
+    if (n == 1)
+    {
+        return;
+    }
+    ReverseRow(n - 1);
+    Console.Write(n + " ");
+}
+
+void printStar(int n,string s)
+{
+    s += '*';
+    Console.WriteLine(s);
+    if (n==1) return;
+    printStar(n-1,s);
+}
+Console.WriteLine("Звезди");
+printStar(7, "");
+
+Console.WriteLine("n n-1 n-2 ... 3 2 1 2 3 ... n-2 n-1 n");
+ReverseRow(20);
+Console.WriteLine();
+
+Console.WriteLine("1 2 3 ... n-2 n-1 n n-1 n-2 ... 3 2 1");
+Row (20,1);
