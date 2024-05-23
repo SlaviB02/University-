fd6432d6214c5edb09f6f81ec24e0e97476561e4
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2024-05-24 00:06:11 +0300

    Add files via upload

---
+++ САА/Домашна3_САА_СлавиБожилов_СИ659.cs
@@ -0,0 +1,38 @@
+﻿
+int[]a= new int[] {0,5,6,8};
+int[] b = new int[] { 1, 2, 3, 4,10 };
+Console.WriteLine(String.Join(",", a));
+Console.WriteLine(String.Join(",", b));
+int[]c=new int[a.Length+b.Length];
+int i=0, j=0,k=0;
+while(i<a.Length && j<b.Length)
+{
+    if (a[i] <= b[j])
+    {
+        c[k] = a[i];
+        i++;
+    }
+    else
+    {
+        c[k] = b[j];
+        j++;
+    }
+    k++;
+   
+}
+
+while (i < a.Length)
+{
+    c[k] = a[i];
+    i++;
+    k++;
+}
+
+while (j < b.Length)
+{
+    c[k] = b[j];
+    j++;
+    k++;
+}
+Console.WriteLine("Слети");
+Console.WriteLine(String.Join(",", c));
